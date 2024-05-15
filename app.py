10c7f5eda25f78bba134831108f0e25305081fd0
el-espaco <168985565+el-espaco@users.noreply.github.com>
2024-05-14 17:53:29 -0700

    checkboxes

--- app.py
+++ app.py
@@ -4,12 +4,32 @@ import streamlit as st
 
 car_data = pd.read_csv('vehicles_us.csv')
 
-st.header('Vehicles Data')
+st.header('Datos de kilometraje de vehiculos')
 
-search_button = st.button('Search')
+# search_button = st.button('Histogram')
 
-if search_button:
-    st.write("Creating Car Mileage Histogram")
+# if search_button:
+#     st.write("Creating Car Mileage Histogram")
 
+#     fig = px.histogram(car_data, x='odometer')
+#     st.plotly_chart(fig, use_container_width=True)
+
+# scatter_button = st.button('Scattered Data')
+
+# if scatter_button:
+#     st.write("Creating Car Mileage Scattered Diagram")
+
+#     fig = px.scatter(car_data, x="odometer", y="price")
+#     st.plotly_chart(fig)
+
+build_histogram = st.checkbox('Construir un histograma')
+build_scatter = st.checkbox('Construir un diagrama de dispersión')
+
+if build_histogram: # si la casilla de verificación está seleccionada
+    st.write('Construir un histograma para la columna odómetro')
     fig = px.histogram(car_data, x='odometer')
     st.plotly_chart(fig, use_container_width=True)
+elif build_scatter:
+    st.write('Construir un diagrama de dispersión de precios y kilometraje')
+    fig = px.scatter(car_data, x="odometer", y="price")
+    st.plotly_chart(fig)
