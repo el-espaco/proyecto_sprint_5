19316fea5d9843c79ce908f8b92c985048344601
el-espaco <168985565+el-espaco@users.noreply.github.com>
2024-05-14 17:03:35 -0700

    pryotect prep config

---
+++ notebooks/app.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+car_data = pd.read_csv('vehicles_us.csv')
+
+st.header('Vehicles Data')
+
+search_button = st.button('Search')
+
+if search_button:
+    st.write("Histogram creation")
+
+    fig = px.histogram(car_data, x='odometer')
+    st.plotly_chart(fig, use_container_width=True)
